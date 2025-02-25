958d414636412fc68cabc848ed8c4141388490a6
ChristianWork05 <christian.lanza@recargaexpress.com>
2025-02-25 16:21:13 -0400

    mejorar diseño del componente BlogComponent: ajustar estructura y estilos para una mejor presentación

--- components/Ayuda/BlogComponent.tsx
+++ components/Ayuda/BlogComponent.tsx
@@ -85,53 +85,53 @@ export default function BlogComponent() {
   };
 
   return (
-    <div className="flex flex-col md:flex-row gap-8 px-4 rounded-md">
-      <div className="w-full md:w-1/4 p-4 bg-white rounded-md shadow-2xl">
-        <ul className="hidden md:block rounded-md">
-          {data?.map((post, index) => (
-            <li
-              key={index}
-              className={
-                selectedPost === post
-                  ? 'flex items-start justify-start pl-2 text-[#6096B9] rounded-md font-semibold text-sm pb-[6px]'
-                  : 'flex items-start justify-start pl-2 hover:bg-gray-200 text-[#444444] rounded-md font-normal text-sm pb-[6px]'
-              }
-              onClick={() => handleTitleClick(post)}
-            >
-              {post.title}
-            </li>
-          ))}
-        </ul>
-        <select
-  className="block md:hidden w-full p-2 border rounded-md text-[#444444] hover:bg-gray-200"
-  onChange={handleSelectChange}
-  value={selectedPost?._id || ''}
->
-  {data?.map((post) => (
-    <option key={post._id} value={post._id} className="text-[#444444]">
-      {post.title}
-    </option>
-  ))}
-</select>
-      </div>
-      <div className="w-full md:w-3/4 flex flex-col p-4 text-[#444444] gap-6">
-        {selectedPost && (
-          <div className="rounded-md p-4">
-            <div className="self-stretch flex items-start justify-start font-bold text-[32px] text-black ">
-              {selectedPost.title}
-            </div>
+<div className="flex flex-col md:flex-row gap-8 px-4 rounded-md max-w-[1200px] mx-auto">
+  <div className="w-full md:w-1/4 h-1/4 flex items-center justify-center p-4 bg-white rounded-md shadow-2xl">
+    <ul className="hidden md:block rounded-md">
+      {data?.map((post, index) => (
+        <li
+          key={index}
+          className={
+            selectedPost === post
+              ? 'flex items-start justify-start pl-2 text-[#6096B9] rounded-md font-semibold text-sm pb-[6px]'
+              : 'flex items-start justify-start pl-2 hover:bg-gray-200 text-[#444444] rounded-md font-normal text-sm pb-[6px]'
+          }
+          onClick={() => handleTitleClick(post)}
+        >
+          {post.title}
+        </li>
+      ))}
+    </ul>
+    <select
+      className="block md:hidden w-full p-2 border rounded-md text-[#444444] hover:bg-gray-200"
+      onChange={handleSelectChange}
+      value={selectedPost?._id || ''}
+    >
+      {data?.map((post) => (
+        <option key={post._id} value={post._id} className="text-[#444444]">
+          {post.title}
+        </option>
+      ))}
+    </select>
+  </div>
+  <div className="w-full md:w-3/4 flex flex-col p-4 text-[#444444] gap-6">
+    {selectedPost && (
+      <div className="rounded-md p-4">
+        <div className="self-stretch flex items-start justify-start font-bold text-[32px] text-black ">
+          {selectedPost.title}
+        </div>
 
-            <div className="self-stretch flex items-start justify-start text-sm font-semibold">
-              {selectedPost.subTitle}
-            </div>
+        <div className="self-stretch flex items-start justify-start text-sm font-semibold">
+          {selectedPost.subTitle}
+        </div>
 
-            <div className="self-stretch flex items-center justify-center text-sm">
-              <Markup content={selectedPost.content} />
-            </div>
-            {/* ... other content */}
-          </div>
-        )}
+        <div className="self-stretch flex items-center justify-center text-sm">
+          <Markup content={selectedPost.content} />
+        </div>
+        {/* ... other content */}
       </div>
-    </div>
+    )}
+  </div>
+</div>
   );
 }
