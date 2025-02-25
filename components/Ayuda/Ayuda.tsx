8d9c689110296fb72fcae3e58fc67d3f2803268c
ChristianWork05 <christian.lanza@recargaexpress.com>
2025-02-25 06:26:34 -0400

    cambiar fondo de la sección Ayuda de gris a blanco

--- components/Ayuda/Ayuda.tsx
+++ components/Ayuda/Ayuda.tsx
@@ -15,7 +15,7 @@ export const Ayuda: FC = () => {
 
     return (
         <>
-            <div className="w-full flex flex-col bg-gray-100 items-center justify-start min-h-screen gap-8 pt-4">
+            <div className="w-full flex flex-col bg-white items-center justify-start min-h-screen gap-8 pt-4">
 <Navbar/>
 
             <div className="flex items-center justify-center">
